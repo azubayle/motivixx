041b131f79c85f7ff44dfdfdf4dd0facd5ae9233
azubayle <59139157+azubayle@users.noreply.github.com>
2023-01-23 10:27:29 +0100

    improve accesibility

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,9 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="FR">
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta
@@ -15,15 +14,25 @@ class MyDocument extends Document {
             property="og:description"
             content="Génère ta lettre de motivation en quelques secondes."
           />
-          <meta property="og:title" content="Génère ta lettre de motivation facilement !" />
+          <meta
+            property="og:title"
+            content="Génère ta lettre de motivation facilement !"
+          />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Génère ta lettre de motivation facilement !" />
+          <meta
+            name="twitter:title"
+            content="Génère ta lettre de motivation facilement !"
+          />
           <meta
             name="twitter:description"
             content="Génère ta lettre de motivation en quelques secondes.."
           />
-   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5264325397086435"
-     crossOrigin="anonymous"></script>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5264325397086435"
+            crossOrigin="anonymous"
+          ></script>
+          <title>Génère ta lettre de motivation gratuitement</title>
         </Head>
         <body>
           <Main />
